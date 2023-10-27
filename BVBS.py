ffdac0f31c87f88bd0e0f1149158275babd630ca
nguyenvanthong308 <52828283+nguyenvanthong308@users.noreply.github.com>
2023-10-27 10:55:56 +0900

    Update BVBS.py

--- BVBS.py
+++ BVBS.py
@@ -873,14 +873,12 @@ def main():
             # Tạo DataFrame mới từ từ điển extracted_numbers và danh sách z_values
             df_extracted = pd.DataFrame(extracted_numbers)
             # Thêm cột mới "z" vào DataFrame với giá trị thứ tự
+            df_extracted.insert(df_extracted.columns.get_loc("l") - 1, "番号", z_values)
             column_SD = df_extracted['SD']
             SD = "SD"
             column_SD_with_prefix = SD + column_SD
             # Cập nhật cột 'l' trong DataFrame
             df_extracted['SD'] = column_SD_with_prefix
-
-            df_extracted.insert(df_extracted.columns.get_loc("d"), "番号", z_values)
-            
             df_extracted.insert(df_extracted.columns.get_loc('s') + 1, 'BVBS', df)
             
             # Đổi tên các cột
